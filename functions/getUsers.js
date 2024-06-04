56cfa50fd225bc007826adef4d517783424e85e2
Korn-Jacob <jackor27@bergen.org>
2024-06-04 10:53:56 -0400

    SEND AND RECIEVE MESSAGE FUNCTIONALITY (when ari puts the changes i made to the azure functions in

---
+++ functions/getUsers.js
@@ -0,0 +1,47 @@
+const { app } = require('@azure/functions');
+
+const sql = require('mssql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+    server: process.env.DB_SERVER,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
+
+app.http('sendMessage', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        context.log(config);
+        try {
+            var poolConnection = await sql.connect(config);
+            context.log(request);
+
+            // SQLI-Proof
+            let result = await poolConnection.request()
+                .input('chatId', sql.Int, request.body.chatId)
+                .query(`SELECT person_id FROM chat_person
+                WHERE chat_id = @chatId`);
+
+            poolConnection.close();
+        } catch (err) {
+            return { body: `failure. Error is ${err}` }
+        }
+
+
+        //const name = request.query.get('name') || await request.text() || 'world';
+
+        return result;
+    }
+});
